78ce4d1e0a2a2570402569180d5096435d5c300b
Tringi <tringi@trimcore.cz>
2020-04-26 15:50:38 +0200

    first commit, private details removed

---
+++ Windows_RwSpinLock.hpp
@@ -0,0 +1,251 @@
+#ifndef WINDOWS_RWSPINLOCK_HPP
+#define WINDOWS_RWSPINLOCK_HPP
+
+#include <Windows.h>
+#include <algorithm>
+#include <cstdint>
+
+namespace Windows {
+    class RwSpinLock;
+    class RwSpinLockScopeShared;
+    class RwSpinLockScopeExclusive;
+
+    // RwSpinLock
+    //  - slim, cross-process, reader-writer spin lock implementation
+    //  - writers don't have priority
+    //
+    class RwSpinLock {
+
+        // state
+        //  - 0 - unowned
+        //  - -1 - owned exclusively (for write/modify operations)
+        //  - +1 and above, number of shared readers
+        //
+        long state = 0;
+
+    private:
+        static constexpr long ExclusivelyOwned = -1;
+        struct Parameters { // NOTE: might need additional tuning
+            struct Exclusive {
+                static constexpr auto Yields = 125u;
+                static constexpr auto Sleep0s = 2u;
+            };
+            struct Shared {
+                static constexpr auto Yields = 120u;
+                static constexpr auto Sleep0s = 7u;
+            };
+            struct Upgrade {
+                static constexpr auto Yields = 27u;
+                static constexpr auto Sleep0s = 100u;
+            };
+        };
+
+    public:
+
+        // C++ style "smart" if-scope operations
+
+        [[nodiscard]] inline RwSpinLockScopeExclusive exclusively () noexcept;
+        [[nodiscard]] inline RwSpinLockScopeExclusive exclusively (std::uint32_t timeout) noexcept;
+
+        [[nodiscard]] inline RwSpinLockScopeShared share () noexcept;
+        [[nodiscard]] inline RwSpinLockScopeShared share (std::uint32_t timeout) noexcept;
+
+    public:
+
+        // simple locking pattern
+
+        inline void acquire () noexcept { this->AcquireExclusive (); }
+        inline void release () noexcept { this->ReleaseExclusive (); }
+
+        [[nodiscard]] inline bool acquire (std::uint32_t timeout) noexcept { return this->AcquireExclusive (timeout); }
+        inline void release (std::uint32_t timeout) noexcept { return this->ReleaseExclusive (); }
+
+    public:
+
+        // full API
+
+        // TryAcquireExclusive
+        //  - attempts to acquire exclusive/write lock, returns result
+        //
+        [[nodiscard]] inline bool TryAcquireExclusive () noexcept {
+            return this->state == 0
+                && InterlockedCompareExchange (&this->state, ExclusivelyOwned, 0) == 0;
+        }
+
+        // TryAcquireShared
+        //  - attempts to acquire shared/read lock, returns result
+        //
+        [[nodiscard]] inline bool TryAcquireShared () noexcept {
+            auto s = this->state;
+            return s != ExclusivelyOwned
+                && InterlockedCompareExchange (&this->state, s + 1, s) == s;
+        }
+
+        // ReleaseExclusive
+        //  - releases all and any locks
+        //
+        inline void ReleaseExclusive () noexcept {
+            InterlockedExchange (&this->state, 0);
+        }
+
+        // ReleaseShared
+        //  - releases one shared/read lock
+        //
+        inline void ReleaseShared () noexcept {
+            InterlockedDecrement (&this->state);
+        }
+
+        // AcquireExclusive
+        //  - acquires the lock for write access (only one thread at a time)
+        //  - thread that owns exclusive lock MUST NOT try to acquire it again
+        //  - the code spins while someone else owns it (not zero) or someone beat us setting it to ExclusivelyOwned in between
+        //     - failing fence in interlocked exchange is allowed, first test is just performance optimization (bus locking)
+        //     - also YieldProcessor (and all other calls) in Spin function is full memory barrier
+        //  - version with timeout parameter returns true on success and false on timeout
+        //  - version without timeout parameter always succeeds or blocks forever (use ForceUnlock to recover)
+        //
+        inline void AcquireExclusive () noexcept;
+
+        [[nodiscard]] inline bool AcquireExclusive (std::uint64_t timeout) noexcept;
+
+        // AcquireShared
+        //  - acquires the lock for read access (multiple threads in parallel, no writter is allowed)
+        //  - nesting reader locks is supported as long as number of acquire and release calls is equal
+        //  - the call spins while someone exclusively owns the lock, the logic for two tests is the same as for AcquireExclusive
+        //  - version with timeout parameter returns true on success and false on timeout
+        //  - version without timeout parameter always succeeds or blocks forever (use ForceUnlock to recover)
+        //
+        inline void AcquireShared () noexcept;
+
+        [[nodiscard]] inline bool AcquireShared (std::uint64_t timeout) noexcept;
+
+        // ForceUnlock
+        //  - use only if the thread/process holding the lock crashed and there is no other reader active
+        //
+        inline void ForceUnlock () noexcept {
+            return this->ReleaseExclusive ();
+        }
+
+        // TryUpgradeToExclusive
+        //  - attempts to convert shared/reading lock to exclusive/writting
+        //  - call ONLY when holding SINGLE shared lock (after successfull AcquireShared/TryAcquireShared)
+        //  - succeeds only if there are no simultaneous readers
+        //
+        [[nodiscard]] inline bool TryUpgradeToExclusive () noexcept {
+            return this->state == 1
+                && InterlockedCompareExchange (&this->state, ExclusivelyOwned, 1) == 1;
+        }
+
+        // UpgradeToExclusive
+        //  - converts shared/reading lock to exclusive/writting
+        //  - call ONLY when holding SINGLE shared lock (after successfull AcquireShared/TryAcquireShared)
+        //
+        inline void UpgradeToExclusive () noexcept;
+
+        [[nodiscard]] inline bool UpgradeToExclusive (std::uint64_t timeout) noexcept;
+
+        // DowngradeToShared
+        //  - converts exclusive/writting lock to shared/reading (allow others to read, while continuing reading)
+        //  - call ONLY when holding exclusive lock, then release using ReleaseShared
+        //
+        inline void DowngradeToShared () noexcept {
+            InterlockedExchange (&this->state, 1);
+        }
+
+    private:
+        template <typename Timings>
+        inline void Spin (std::uint32_t round);
+    };
+
+    // RwSpinLockScopeExclusive
+    //  - unlocks exlusive lock acquired through RwSpinLock::exclusively
+    //
+    class RwSpinLockScopeExclusive {
+        friend class RwSpinLock;
+        RwSpinLock * lock;
+
+        inline RwSpinLockScopeExclusive (RwSpinLock * lock) noexcept : lock (lock) {};
+
+    public:
+
+        // movable
+
+        inline RwSpinLockScopeExclusive (RwSpinLockScopeExclusive && from) noexcept : lock (from.lock) { from.lock = nullptr; }
+        inline RwSpinLockScopeExclusive & operator = (RwSpinLockScopeExclusive && from) noexcept { std::swap (this->lock, from.lock); return *this; }
+
+        // release lock on destruction
+
+        inline ~RwSpinLockScopeExclusive () noexcept;
+
+        // release
+        //  - to manually release the exclusive lock before going out of scope
+        //  - not checking for null to early catch bugs
+        //
+        inline void release () noexcept;
+
+        // operator bool
+        //  - returns whether the exclusive lock is still active
+        //  - enables use in 'if' expression to introduce local scope
+        //
+        explicit operator bool () const & {
+            return this->lock != nullptr;
+        }
+
+#ifndef __INTELLISENSE__
+        // operator bool, invalid call
+        //  - using "if (lock.exclusive ())" is bug -> use "if (auto x = lock.exclusive ())" instead
+        //
+        explicit operator bool () const && = delete;
+#endif
+    };
+
+    // RwSpinLockScopeShared
+    //  - unlocks exlusive lock acquired through RwSpinLock::shared
+    //
+    class RwSpinLockScopeShared {
+        friend class RwSpinLock;
+        RwSpinLock * lock;
+
+        inline RwSpinLockScopeShared (RwSpinLock * lock) noexcept : lock (lock) {};
+
+    public:
+
+        // copyable
+
+        inline RwSpinLockScopeShared (const RwSpinLockScopeShared & from) noexcept;
+        inline RwSpinLockScopeShared & operator = (const RwSpinLockScopeShared & from) noexcept;
+
+        // movable
+
+        inline RwSpinLockScopeShared (RwSpinLockScopeShared && from) noexcept : lock (from.lock) { from.lock = nullptr; }
+        inline RwSpinLockScopeShared & operator = (RwSpinLockScopeShared && from) noexcept { std::swap (this->lock, from.lock); return *this; }
+
+        // release lock on destruction
+
+        inline ~RwSpinLockScopeShared () noexcept;
+
+        // release
+        //  - to manually release the exclusive lock before going out of scope
+        //  - not checking for null to early catch bugs
+        //
+        inline void release () noexcept;
+
+        // operator bool
+        //  - returns whether the lock is still active
+        //  - enables use in 'if' expression to introduce local scope
+        //
+        explicit operator bool () const & {
+            return this->lock != nullptr;
+        }
+
+#ifndef __INTELLISENSE__
+        // operator bool, invalid call
+        //  - using "if (lock.shared ())" is bug -> use "if (auto x = lock.shared ())" instead
+        //
+        explicit operator bool () const && = delete;
+#endif
+    };
+}
+
+#include "Windows_RwSpinLock.tcc"
+#endif
---
+++ Windows_RwSpinLock.tcc
@@ -0,0 +1,187 @@
+#ifndef WINDOWS_RWSPINLOCK_TCC
+#define WINDOWS_RWSPINLOCK_TCC
+
+// RwSpinLockScopeExclusive
+
+inline Windows::RwSpinLockScopeExclusive::~RwSpinLockScopeExclusive () noexcept {
+    if (this->lock) {
+        this->release ();
+    }
+}
+
+inline void Windows::RwSpinLockScopeExclusive::release () noexcept {
+    this->lock->ReleaseExclusive ();
+    this->lock = nullptr;
+}
+
+// RwSpinLockScopeShared
+
+inline Windows::RwSpinLockScopeShared::RwSpinLockScopeShared (const Windows::RwSpinLockScopeShared & from) noexcept : lock (from.lock) { this->lock->AcquireShared (); }
+inline Windows::RwSpinLockScopeShared & Windows::RwSpinLockScopeShared::operator = (const Windows::RwSpinLockScopeShared & from) noexcept {
+    if (this->lock) {
+        this->release ();
+    }
+    this->lock = from.lock;
+    if (this->lock) {
+        this->lock->AcquireShared ();
+    }
+    return *this;
+}
+
+inline Windows::RwSpinLockScopeShared::~RwSpinLockScopeShared () noexcept {
+    if (this->lock) {
+        this->release ();
+    }
+}
+
+inline void Windows::RwSpinLockScopeShared::release () noexcept {
+    this->lock->ReleaseShared ();
+    this->lock = nullptr;
+}
+
+// RwSpinLock
+
+inline void Windows::RwSpinLock::AcquireExclusive () noexcept {
+    std::uint32_t r = 0;
+    while (!this->TryAcquireExclusive ()) {
+        if (++r <= Parameters::Exclusive::Yields) {
+            YieldProcessor ();
+        } else {
+            this->Spin <Parameters::Exclusive> (r);
+        }
+    }
+}
+
+inline void Windows::RwSpinLock::AcquireShared () noexcept {
+    std::uint32_t r = 0;
+    while (!this->TryAcquireShared ()) {
+        if (++r <= Parameters::Shared::Yields) {
+            YieldProcessor ();
+        } else {
+            this->Spin <Parameters::Shared> (r);
+        }
+    }
+}
+
+inline void Windows::RwSpinLock::UpgradeToExclusive () noexcept {
+    std::uint32_t r = 0;
+    while (!this->TryUpgradeToExclusive ()) {
+        if (++r <= Parameters::Upgrade::Yields) {
+            YieldProcessor ();
+        } else {
+            this->Spin <Parameters::Upgrade> (r);
+        }
+    }
+}
+
+[[nodiscard]] inline bool Windows::RwSpinLock::AcquireExclusive (std::uint64_t timeout) noexcept {
+    std::uint32_t r = 0;
+
+    while (!this->TryAcquireExclusive ()) {
+        if (++r <= Parameters::Exclusive::Yields) {
+            YieldProcessor ();
+        } else {
+            if (timeout) {
+                auto t = GetTickCount64 () + timeout;
+                SwitchToThread ();
+
+                // contested case, with backoff
+                while (!this->TryAcquireExclusive ()) {
+                    if (GetTickCount64 () < t) {
+                        this->Spin <Parameters::Exclusive> (++r);
+                    } else
+                        return false;
+                }
+            }
+            break;
+        }
+    }
+    return true;
+}
+
+[[nodiscard]] inline bool Windows::RwSpinLock::AcquireShared (std::uint64_t timeout) noexcept {
+    std::uint32_t r = 0;
+
+    while (!this->TryAcquireShared ()) {
+        if (++r <= Parameters::Shared::Yields) {
+            YieldProcessor ();
+        } else {
+            if (timeout) {
+                auto t = GetTickCount64 () + timeout;
+                SwitchToThread ();
+
+                // contested case, with backoff
+                while (!this->TryAcquireShared ()) {
+                    if (GetTickCount64 () < t) {
+                        this->Spin <Parameters::Shared> (++r);
+                    } else
+                        return false;
+                }
+            }
+            break;
+        }
+    }
+    return true;
+}
+
+[[nodiscard]] inline bool Windows::RwSpinLock::UpgradeToExclusive (std::uint64_t timeout) noexcept {
+    std::uint32_t r = 0;
+
+    while (!this->TryUpgradeToExclusive ()) {
+        if (++r <= Parameters::Upgrade::Yields) {
+            YieldProcessor ();
+        } else {
+            if (timeout) {
+                auto t = GetTickCount64 () + timeout;
+                SwitchToThread ();
+
+                // contested case, with backoff
+                while (!this->TryUpgradeToExclusive ()) {
+                    if (GetTickCount64 () < t) {
+                        this->Spin <Parameters::Upgrade> (++r);
+                    } else
+                        return false;
+                }
+            }
+            break;
+        }
+    }
+    return true;
+}
+
+// internals
+
+template <typename Timings>
+inline void Windows::RwSpinLock::Spin (std::uint32_t round) {
+    DWORD n = 0;
+    if (round > Timings::Yields + Timings::Sleep0s) {
+        n = 1;
+    }
+    Sleep (n);
+}
+
+// if scope
+
+[[nodiscard]] inline Windows::RwSpinLockScopeExclusive Windows::RwSpinLock::exclusively () noexcept {
+    this->AcquireExclusive ();
+    return this;
+}
+[[nodiscard]] inline Windows::RwSpinLockScopeExclusive Windows::RwSpinLock::exclusively (std::uint32_t timeout) noexcept {
+    if (this->AcquireExclusive (timeout))
+        return this;
+    else
+        return nullptr;
+}
+
+[[nodiscard]] inline Windows::RwSpinLockScopeShared Windows::RwSpinLock::share () noexcept {
+    this->AcquireShared ();
+    return this;
+}
+[[nodiscard]] inline Windows::RwSpinLockScopeShared Windows::RwSpinLock::share (std::uint32_t timeout) noexcept {
+    if (this->AcquireShared (timeout))
+        return this;
+    else
+        return nullptr;
+}
+
+#endif
